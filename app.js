d5307d2cfba71083eadf060bb21ef4f5c457a0da
Vishwanath Veerni <vishwanath.veerni@gmail.com>
2022-07-04 22:00:13 +0530

    First commit

---
+++ app.js
@@ -0,0 +1,52 @@
+const express = require("express");
+const app = express();
+
+const client = require("@mailchimp/mailchimp_marketing");
+
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/signup.html");
+});
+
+app.post("/", (req, res) => {
+  const email = req.body.Email;
+  const fname = req.body.Fname;
+  const lname = req.body.Lname;
+
+  client.setConfig({
+    apiKey: "a970fa217d44725a4bac718e920e4d49",
+    server: "us11",
+  });
+
+  const run = async () => {
+    try {
+      const response = await client.lists.batchListMembers("qc4872b8f39", {
+        members: [
+          {
+            email_address: email,
+            status: "subscribed",
+            merge_fields: {
+              FNAME: fname,
+              LNAME: lname,
+            },
+          },
+        ],
+      });
+      res.sendFile(__dirname + "/success.html");
+    } catch (error) {
+      var errorCode = error.response.body.status;
+      var errorDetail = error.response.body.detail;
+      res.sendFile(__dirname + "/failure.html");
+    }
+  };
+
+  run();
+});
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log("Server is running at port 3000");
+});
+    
